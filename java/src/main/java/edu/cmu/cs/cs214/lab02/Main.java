08469aa7d19c6aca67fa14fd21fef615756ee734
v少爷 <nogo@vshaoyedembp.wifi.local.cmu.edu>
2023-09-05 17:55:37 -0400

    Encapsulation

--- java/src/main/java/edu/cmu/cs/cs214/lab02/Main.java
+++ java/src/main/java/edu/cmu/cs/cs214/lab02/Main.java
@@ -1,12 +1,13 @@
 package edu.cmu.cs.cs214.lab02;
 
-import edu.cmu.cs.cs214.lab02.shapes.Rectangle;
+import edu.cmu.cs.cs214.lab02.shapes.*;
 
 public class Main {
     public static void main(String[] args) {
-        Rectangle rectangle = new Rectangle(2, 3);
-
-        Renderer renderer = new Renderer(rectangle);
+      // Rectangle rectangle = new Rectangle(2, 3);
+       //Circle circle=new Circle(3);
+       Square square=new Square(3);
+        Renderer renderer = new Renderer(square);
 
         renderer.draw();
     }
